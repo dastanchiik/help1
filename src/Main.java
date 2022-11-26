3fd66f79bf8b755cda70481de38b8cf65ce295b2
dastanchick <dosyafree32@gmail.com>
2022-11-26 19:17:28 +0600

    I added sub and string

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
        Scanner scanner = new Scanner(System.in);
        String name = scanner.next();
+
        int count = name.length();
        int counter = 0;
         while (counter != count){
