8830fea87fbd903b8960247681f17b09c6ed4ec5
dastanchick <dosyafree32@gmail.com>
2022-11-26 19:18:57 +0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
     public static void main(String[] args) {
        Scanner scanner = new Scanner(System.in);
        String name = scanner.next();
-
        int count = name.length();
        int counter = 0;
         while (counter != count){
