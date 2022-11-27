686c9183f38739e61b3bb7a7990607eeab2aea59
dastanchick <dosyafree32@gmail.com>
2022-11-27 15:13:18 +0600

    I added sub and string

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
        Scanner scanner = new Scanner(System.in);
-       String name = scanner.next();
-       int count = name.length();
-       int counter = 0;
-        while (counter != count){
-            System.out.println(name.substring(counter));
-       counter++;
-        }
+      String a = scanner.next();
+      int count = a.length();
+     int counter = 0;
+     while (counter != count){
+         System.out.println(a.substring(counter));
+         counter++;
+     }
     }
         }
-
