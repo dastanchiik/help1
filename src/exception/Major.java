a99899e4f871156e490bf8969e2dcb480393ee9a
dastanchick <dosyafree32@gmail.com>
2022-12-21 16:23:11 +0600

    I added all

---
+++ src/exception/Major.java
@@ -0,0 +1,21 @@
+package exception;
+
+import java.util.Scanner;
+
+public class Major {
+    public static void main(String[] args) {
+    method();
+    }
+    public static void method(){
+        Scanner scanner = new Scanner(System.in);
+        try {
+        System.out.print("Enter name:");
+        String name = scanner.next();
+        System.out.print("Enter age:");
+        int age = scanner.nextInt();
+        }catch (Exception e){
+//            System.out.println(e.getMessage());
+            System.out.println("\n wrong");
+        }
+    }
+}
