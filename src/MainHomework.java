914110f47ba04af3b4ccbd9ed38c98a0f10d74f5
dastanchick <dosyafree32@gmail.com>
2022-12-24 00:51:06 +0600

    I added all

--- src/MainHomework.java
+++ src/MainHomework.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 
 public class MainHomework {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         method();
     }
 
     public static void method() {
         Scanner scanner = new Scanner(System.in);
         Cat cat = new Cat(scanner.next(), scanner.nextInt());
+        if (cat.age()<0) {
+            System.out.println(cat.name());
+        }else {
+            System.out.println(cat);
+        }
     }
 }
