358f4a0c801a81d450291ed41680d2957404986f
dastanchick <dosyafree32@gmail.com>
2022-12-28 19:54:54 +0600

    I added all

--- src/MainHomework.java
+++ src/MainHomework.java
@@ -1,26 +1,24 @@
-import java.util.Scanner;
 public class MainHomework {
     public static void main(String[] args) {
-        int a = 123;
-       String x = String.valueOf(a);
-//    int b = Integer.valueOf();
-//    int c = b;
-//        System.out.println(c);
-        //method(2.00000,10);
+        int a = -123;
+        System.out.println(method(a));
     }
-//    public static int method(double name,int age) {
-//      String names = String.valueOf(age);
-//         String o = String.valueOf(age);
-//         StringBuilder stringBuilder = new StringBuilder();
-//         StringBuilder z = stringBuilder.reverse();
-//         int ages = Integer.valueOf(o);
-//    }
-//    public static void method2(int a,int b,int c){
-//        System.out.println(a+""+b+""+c);
-//    }
+    public static int method(int a) {
+        int q = (int)Math.sqrt(a);
+        return q;
+        //        StringBuilder stringBuilder = new StringBuilder();
+//        char[] chars = String.valueOf(a).toCharArray();
+//        for (int i = chars.length - 1; i >= 0; i--) {
+//            stringBuilder.append(chars[i]);
+//        }
+//        int firstNumber = Integer.parseInt(String.valueOf(stringBuilder));
+//        String s = String.valueOf(stringBuilder);
+//        int result = 0;
+//        if (firstNumber<0){
+//            result = Integer.parseInt(String.valueOf(result).substring(s.length()-1));
+//            int pars = Integer.parseInt("-"+result);
+//            return pars == a;
+//        }
+//        return firstNumber == a;
+    }
 }
-
-
-
-
-
