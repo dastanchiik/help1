6e50da536b93d5ff2085325e0970c580ffcc2b1a
dastanchick <dosyafree32@gmail.com>
2023-01-07 23:43:03 +0600

    It's new

--- src/MainHomework.java
+++ src/MainHomework.java
@@ -1,23 +1,11 @@
+import java.util.ArrayList;
 
 public class MainHomework {
     public static void main(String[] args) {
         int a = -123;
     }
-    public boolean isMatch(String s, String p) {
-        System.out.println( new StringBuilder(String.valueOf(s)).reverse());
-            StringBuilder stringBuilder = new StringBuilder();
-        char[] chars = String.valueOf(a).toCharArray();
-        for (int i = chars.length - 1; i >= 0; i--) {
-            stringBuilder.append(chars[i]);
-        }
-        int firstNumber = Integer.parseInt(String.valueOf(stringBuilder));
-        String s = String.valueOf(stringBuilder);
-        int result = 0;
-        if (firstNumber<0){
-            result = Integer.parseInt(String.valueOf(result).substring(s.length()-1));
-            int pars = Integer.parseInt("-"+result);
-            return pars == a;
-        }
-        return firstNumber == a;
+    public void isMatch(ArrayList<Integer> list, String p) {
+//        ArrayList<Integer>list = new ArrayList<>();
+        list.stream().filter(x -> false).forEach(System.out::println);
     }
 }
