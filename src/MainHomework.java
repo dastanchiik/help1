73962d73525f568d6e1eaeb824c154a09a14af5f
dastanchick <dosyafree32@gmail.com>
2023-01-07 23:37:58 +0600

    It's new

--- src/MainHomework.java
+++ src/MainHomework.java
@@ -4,22 +4,20 @@ public class MainHomework {
         int a = -123;
     }
     public boolean isMatch(String s, String p) {
-       return true;
-
-//        System.out.println( new StringBuilder(String.valueOf(s)).reverse());
-    //        StringBuilder stringBuilder = new StringBuilder();
-//        char[] chars = String.valueOf(a).toCharArray();
-//        for (int i = chars.length - 1; i >= 0; i--) {
-//            stringBuilder.append(chars[i]);
-//        }
-//        int firstNumber = Integer.parseInt(String.valueOf(stringBuilder));
-//        String s = String.valueOf(stringBuilder);
-//        int result = 0;
-//        if (firstNumber<0){
-//            result = Integer.parseInt(String.valueOf(result).substring(s.length()-1));
-//            int pars = Integer.parseInt("-"+result);
-//            return pars == a;
-//        }
-//        return firstNumber == a;
+        System.out.println( new StringBuilder(String.valueOf(s)).reverse());
+            StringBuilder stringBuilder = new StringBuilder();
+        char[] chars = String.valueOf(a).toCharArray();
+        for (int i = chars.length - 1; i >= 0; i--) {
+            stringBuilder.append(chars[i]);
+        }
+        int firstNumber = Integer.parseInt(String.valueOf(stringBuilder));
+        String s = String.valueOf(stringBuilder);
+        int result = 0;
+        if (firstNumber<0){
+            result = Integer.parseInt(String.valueOf(result).substring(s.length()-1));
+            int pars = Integer.parseInt("-"+result);
+            return pars == a;
+        }
+        return firstNumber == a;
     }
 }
